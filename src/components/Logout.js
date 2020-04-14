34d3ed5879f98ccb3714170e533a86c9470d27bb
Pulse6 <marco698698@hotmail.com>
2020-04-14 14:46:54 -0400

    logout will close session

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./styles.scss";
+import axios from "axios";
 
 function Logout(props) {
+
+  const closeUser = () => {
+    axios.post
+      ('/logout')
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((error) => console.error(error));
+  }
 
   return (
     <button
     onClick={e => {
       localStorage.removeItem('user');
       props.setUser("");
+      closeUser();
     }}>
       Logout
     </button>
