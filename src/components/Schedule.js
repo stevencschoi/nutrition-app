0e23eb954b4840356b23070f7eb7cca8d694a61b
stevencschoi <stevencschoi87@gmail.com>
2020-04-15 21:24:37 -0400

    formatting moment

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -2,24 +2,39 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import ScheduleItem from "./ScheduleItem";
+import MealCalendar from "./MealCalendar";
+import moment from "moment";
+
+// moment(testDate).format('MM/DD/YYYY');
 
 function Schedule() {
   const [cal, setCal] = useState(null);
+  const [currentDay, setCurrentDay] = useState(moment());
 
   useEffect(() => {
-    let newDate = new Date();
-    const date = newDate.getDate();
-    const month = newDate.getMonth() + 1;
-    const year = newDate.getFullYear();
-    const separator = "-";
+    console.log("todayyyyyyyyyy: ", currentDay.format("YYYY-MM-DD"));
+    // console.log("in _d: ", currentDay._d);
+    // const formatdate = JSON.stringify(currentDay._d).slice(1, 11);
+    // console.log("formatdate", formatdate);
 
-    const today = `${year}${separator}${
-      month < 10 ? `0${month}` : `${month}`
-    }${separator}${date}`;
+    // let newDate = new Date();
+    // const date = newDate.getDate();
+    // const month = newDate.getMonth() + 1;
 
+    // if (month > 12) {
+    //   month = 1;
+    // }
+
+    // console.log(month);
+    // const year = newDate.getFullYear();
+    // const separator = "-";
+
+    // const today = `${year}${separator}${
+    //   month < 10 ? `0${month}` : `${month}`
+    // }${separator}${date}`;
     // console.log("today is ", shit);
-    getDaySchedule(today);
-  }, []);
+    getDaySchedule(currentDay.format("YYYY-MM-DD"));
+  }, [currentDay]);
 
   const getDaySchedule = (currentDate) => {
     // const userId = Cookies.get("userId");
@@ -28,7 +43,7 @@ function Schedule() {
     axios
       .get(`/day?date=${currentDate}`)
       .then((result) => {
-        console.log("this data", result.data);
+        // console.log("this data", result.data);
         const itemarr = result.data.map((item) => {
           const itemName = item.recipe_name;
           const image = item.image;
@@ -39,10 +54,12 @@ function Schedule() {
       })
       .catch((error) => console.error(error));
   };
-
   return (
     <>
-      <h2>Data</h2>
+      <MealCalendar
+        date={currentDay}
+        onChange={(e) => setCurrentDay(e.target.value)}
+      />
       {cal && cal}
     </>
   );
