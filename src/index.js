02730ba7ab98ceabab0b50f4f81063e3d0331a81
Victor <victorcwyu@gmail.com>
2020-05-13 14:42:30 -0400

    Initial deployment files added.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
