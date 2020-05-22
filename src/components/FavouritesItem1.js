9f1076e8c882bc37ffd51500b6e11f5a1bbaacc4
Victor <victorcwyu@gmail.com>
2020-05-22 15:48:48 -0400

    Favourites item now responsive in same manner as recipe view.

--- src/components/FavouritesItem1.js
+++ src/components/FavouritesItem1.js
@@ -1,10 +1,162 @@
+// import React, { useState } from "react";
+// import "./styles.scss";
+// import axios from "axios";
+// import FavouritesItemGraph from "./FavouritesItemGraph";
+// import MealCalendar from "./MealCalendar";
+// import Button from "./Button";
+// import { Dropdown } from "semantic-ui-react";
+// import { socket } from "../hooks/useApplicationData";
+
+// const FavouritesItem1 = (props) => {
+//   const [date, setDate] = useState(null);
+//   const [meal, setMeal] = useState(null);
+
+//   const deleteFav = () => {
+//     const id = props.id;
+
+//     axios
+//       .post("/favourites/delete", { recipeId: id })
+//       .then((result) => {
+//         props.setUpdateItem(id);
+//       })
+//       .catch((error) => console.error(error));
+//   };
+
+//   // add a recipe to a given day
+//   const addRecipeToDay = (id, date, meal) => {
+//     const formatdate = JSON.stringify(date).slice(1, 11);
+//     const recipeId = id;
+
+//     axios
+//       .post("/day/add", {
+//         date: `${formatdate}`,
+//         recipeId: `${recipeId}`,
+//         mealNumber: `${meal}`,
+//       })
+//       .then((result) => {
+//         socket.emit("new", (data) => {
+//           console.log("Socket sending from addrecipe", data);
+//         });
+//         setDate(null);
+//       })
+//       .catch((error) => console.error(error));
+//   };
+
+//   const options = [
+//     { key: 1, text: "Breakfast", value: "1" },
+//     { key: 2, text: "Lunch", value: "2" },
+//     { key: 3, text: "Dinner", value: "3" },
+//     { key: 4, text: "Other", value: "4" },
+//   ];
+
+//   return (
+//     <>
+//       <div className="FavouritesItem">
+//         <div className="favourite-main">
+//           <div className="image-and-drop-container">
+//             <div className="image-container">
+//               <a href={`/recipe/${props.name}`}>
+//                 <img src={props.image_url} />
+//               </a>
+//               {props.id && (
+//                 <Button
+//                   deleteImage
+//                   onClick={() => {
+//                     deleteFav();
+//                   }}
+//                 >
+//                   <i class="far fa-trash-alt"></i>
+//                 </Button>
+//               )}
+//             </div>
+//           </div>
+//             <FavouritesItemGraph
+//               calories={props.calories}
+//               fat_in_g={props.fat_in_g}
+//               carbs_in_g={props.carbs_in_g}
+//               protein_in_g={props.protein_in_g}
+//               sugar_in_g={props.sugar_in_g}
+//               fiber_in_g={props.fiber_in_g}
+//               cholesterol_in_mg={props.cholesterol_in_mg}
+//               sodium_in_mg={props.sodium_in_mg}
+//               image_url={props.image_url}
+//               name={props.name}
+//               recipe_yield={props.recipe_yield}
+//             />
+//         </div>
+//         <div className="meal-plan-container">
+//           <div className="plan-title">
+//             <h3>
+//               <i class="far fa-calendar-alt"></i> Add to Your Meal Plan
+//             </h3>
+//           </div>
+//           <div className="dropdown-container">
+//             <div className="selectContainer">
+//               <div className="selectPosition">
+//                 <MealCalendar
+//                   date={date}
+//                   onChange={(e) => setDate(e.target.value)}
+//                 />
+//                 <div>
+//                   {date && (
+//                     <Dropdown
+//                       options={options}
+//                       selection
+//                       onChange={(e, { value }) => setMeal(value)}
+//                     />
+//                   )}
+//                 </div>
+//                 {date && meal && (
+//                   <div className="fav-button-container">
+//                     <div className="add">
+//                       <Button
+//                         onClick={() => {
+//                           addRecipeToDay(props.id, date, meal);
+//                           setMeal(null);
+//                         }}
+//                       >
+//                         Add
+//                       </Button>
+//                     </div>
+//                     <div>
+//                       <Button
+//                         delete
+//                         onClick={() => {
+//                           setDate(null);
+//                           setMeal(null);
+//                         }}
+//                       >
+//                         Cancel
+//                       </Button>
+//                     </div>
+//                   </div>
+//                 )}
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default FavouritesItem1;
+
+
+
+
+
+
+
+
+
+
 import React, { useState } from "react";
 import "./styles.scss";
 import axios from "axios";
 import FavouritesItemGraph from "./FavouritesItemGraph";
 import MealCalendar from "./MealCalendar";
 import Button from "./Button";
-import { Dropdown } from "semantic-ui-react";
 import { socket } from "../hooks/useApplicationData";
 
 const FavouritesItem1 = (props) => {
@@ -31,7 +183,6 @@ const FavouritesItem1 = (props) => {
       .post("/day/add", {
         date: `${formatdate}`,
         recipeId: `${recipeId}`,
-        mealNumber: `${meal}`,
       })
       .then((result) => {
         socket.emit("new", (data) => {
@@ -42,18 +193,16 @@ const FavouritesItem1 = (props) => {
       .catch((error) => console.error(error));
   };
 
-  const options = [
-    { key: 1, text: "Breakfast", value: "1" },
-    { key: 2, text: "Lunch", value: "2" },
-    { key: 3, text: "Dinner", value: "3" },
-    { key: 4, text: "Other", value: "4" },
-  ];
-
   return (
     <>
       <div className="FavouritesItem">
+        <div className="including-link">
+
         <div className="favourite-main">
           <div className="image-and-drop-container">
+
+
+            
             <div className="image-container">
               <a href={`/recipe/${props.name}`}>
                 <img src={props.image_url} />
@@ -69,7 +218,31 @@ const FavouritesItem1 = (props) => {
                 </Button>
               )}
             </div>
+
+
+              <div class="ingredient-data">
+                <h2>Select Nutritional Data of {props.name}</h2>
+                <FavouritesItemGraph
+                  calories={props.calories}
+                  fat_in_g={props.fat_in_g}
+                  carbs_in_g={props.carbs_in_g}
+                  protein_in_g={props.protein_in_g}
+                  sugar_in_g={props.sugar_in_g}
+                  fiber_in_g={props.fiber_in_g}
+                  cholesterol_in_mg={props.cholesterol_in_mg}
+                  sodium_in_mg={props.sodium_in_mg}
+                  image_url={props.image_url}
+                  name={props.name}
+                  recipe_yield={props.recipe_yield}
+                />
+              </div>
+
+
+
+
           </div>
+          {/* <div class="ingredient-data">
+            <h2>Select Nutritional Data of {props.name}</h2>
             <FavouritesItemGraph
               calories={props.calories}
               fat_in_g={props.fat_in_g}
@@ -83,7 +256,13 @@ const FavouritesItem1 = (props) => {
               name={props.name}
               recipe_yield={props.recipe_yield}
             />
+          </div> */}
         </div>
+
+
+
+
+        
         <div className="meal-plan-container">
           <div className="plan-title">
             <h3>
@@ -95,30 +274,24 @@ const FavouritesItem1 = (props) => {
               <div className="selectPosition">
                 <MealCalendar
                   date={date}
-                  onChange={(e) => setDate(e.target.value)}
+                  onChange={(e) => {
+                    setDate(e.target.value)
+                    setMeal("yum")
+                  }}
                 />
-                <div>
-                  {date && (
-                    <Dropdown
-                      options={options}
-                      selection
-                      onChange={(e, { value }) => setMeal(value)}
-                    />
-                  )}
-                </div>
                 {date && meal && (
                   <div className="fav-button-container">
                     <div className="add">
                       <Button
                         onClick={() => {
-                          addRecipeToDay(props.id, date, meal);
+                          addRecipeToDay(props.id, date);
                           setMeal(null);
                         }}
                       >
                         Add
                       </Button>
                     </div>
-                    <div>
+                    <div className="delete">
                       <Button
                         delete
                         onClick={() => {
@@ -134,6 +307,7 @@ const FavouritesItem1 = (props) => {
               </div>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </>
--- src/components/FavouritesItemGraph.js
+++ src/components/FavouritesItemGraph.js
@@ -1,3 +1,140 @@
+// import React from "react";
+// import "./styles.scss";
+// import {
+//   BarChart,
+//   Bar,
+//   XAxis,
+//   YAxis,
+//   CartesianGrid,
+//   Tooltip,
+//   ResponsiveContainer,
+// } from "recharts";
+// function RecipeGraph(props) {
+//   const makeGraph = (
+//     calories,
+//     fat_in_g,
+//     carbs_in_g,
+//     protein_in_g,
+//     sugar_in_g,
+//     fiber_in_g,
+//     cholesterol_in_mg,
+//     sodium_in_mg,
+//     image_url,
+//     recipe_yield
+//   ) => {
+//     let fat = parseFloat(fat_in_g);
+//     let carbohydrates = parseFloat(carbs_in_g);
+//     let protein = parseFloat(protein_in_g);
+//     let cholesterol = parseFloat(cholesterol_in_mg);
+//     let sodium = parseFloat(sodium_in_mg);
+//     let sugar = parseFloat(sugar_in_g);
+//     let fibre = parseFloat(fiber_in_g);
+//     const data = [
+//       {
+//         name: "Fat",
+//         "grams / serving": fat,
+//         fill: "#83a6ed",
+//       },
+//       {
+//         name: "Carbohydrates",
+//         "grams / serving": carbohydrates,
+//         fill: "#8dd1e1",
+//       },
+//       {
+//         name: "Protein",
+//         "grams / serving": protein,
+//         fill: "#82ca9d",
+//       },
+//       {
+//         name: "Sugar",
+//         "grams / serving": sugar,
+//         fill: "#ffc658",
+//       },
+//       {
+//         name: "Fibre",
+//         "grams / serving": fibre,
+//         fill: "#8C564B",
+//       },
+//     ];
+//     const data2 = [
+//       {
+//         name: "Cholesterol",
+//         "milligrams / serving": cholesterol,
+//         fill: "#a4de6c",
+//       },
+//       {
+//         name: "Sodium",
+//         "milligrams / serving": sodium,
+//         fill: "#d0ed57",
+//       },
+//     ];
+//     return (
+//       // favourites graphs
+//       <div class="ingredient-graph-container">
+//         <div class="left-graph">
+//           <ResponsiveContainer width="100%" height="100%">
+//             <BarChart width={500} height={300} data={data}>
+//               <CartesianGrid strokeDasharray="3 3" />
+//               <XAxis dataKey="name" />
+//               <YAxis
+//                 label={{
+//                   value: "grams / serving",
+//                   dx: -10,
+//                   angle: -90,
+//                   position: "center",
+//                 }}
+//                 type="number"
+//               />
+//               <Tooltip />
+//               <Bar dataKey="grams / serving" />
+//             </BarChart>
+//           </ResponsiveContainer>
+//         </div>
+//         <div class="right-graph">
+//           <ResponsiveContainer width="100%" height="100%">
+//             <BarChart width={240} height={300} data={data2}>
+//               <CartesianGrid strokeDasharray="3 3" />
+//               <XAxis dataKey="name" />
+//               <YAxis
+//                 label={{
+//                   value: "milligrams / serving",
+//                   dx: -20,
+//                   angle: -90,
+//                   position: "center",
+//                 }}
+//                 type="number"
+//               />
+//               <Tooltip />
+//               <Bar dataKey="milligrams / serving" />
+//             </BarChart>
+//           </ResponsiveContainer>
+//         </div>
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <div class="favourites-data">
+//       <h2>Select Nutritional Data of {props.name}</h2>
+//       <div>
+//         {makeGraph(
+//           props.calories,
+//           props.fat_in_g,
+//           props.carbs_in_g,
+//           props.protein_in_g,
+//           props.sugar_in_g,
+//           props.fiber_in_g,
+//           props.cholesterol_in_mg,
+//           props.sodium_in_mg,
+//           props.image_url,
+//           props.recipe_yield
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+// export default RecipeGraph;
+
 import React from "react";
 import "./styles.scss";
 import {
@@ -114,22 +251,19 @@ function RecipeGraph(props) {
   };
 
   return (
-    <div class="favourites-data">
-      <h2>Select Nutritional Data of {props.name}</h2>
-      <div>
-        {makeGraph(
-          props.calories,
-          props.fat_in_g,
-          props.carbs_in_g,
-          props.protein_in_g,
-          props.sugar_in_g,
-          props.fiber_in_g,
-          props.cholesterol_in_mg,
-          props.sodium_in_mg,
-          props.image_url,
-          props.recipe_yield
-        )}
-      </div>
+    <div>
+      {makeGraph(
+        props.calories,
+        props.fat_in_g,
+        props.carbs_in_g,
+        props.protein_in_g,
+        props.sugar_in_g,
+        props.fiber_in_g,
+        props.cholesterol_in_mg,
+        props.sodium_in_mg,
+        props.image_url,
+        props.recipe_yield
+      )}
     </div>
   );
 }
