8140c5cebb6d04a1cd66f2a40878f4dd372a6743
Victor <victorcwyu@gmail.com>
2020-05-29 00:03:09 -0400

    Nav now responsive and renders conditionally.

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,69 +1,3 @@
-// import React, { useState } from "react";
-// import "../styles.scss";
-// import Login from "./Login";
-// import Logout from "./Logout";
-// import { Link } from "react-router-dom";
-
-// function Nav() {
-//   const [user, setUser] = useState(localStorage.getItem("user"));
-
-//   return (
-//     <nav className="nav">
-//       <Link to="/">
-//         <h3 id="logo">
-//           Veg<i className="fas fa-seedling"></i>able
-//         </h3>
-//       </Link>
-
-
-
-//       <div className="media">
-//         <Link to="/favourites">
-//           <h3>
-//             <i className="far fa-heart"></i> Favourites
-//           </h3>
-//         </Link>
-//         <Link to="/schedule">
-//           <h3>
-//             <i className="far fa-calendar-alt"></i> Schedule
-//           </h3>
-//         </Link>
-//         <Link to="/data">
-//           <h3>
-//             <i className="fas fa-chart-line"></i> Data
-//           </h3>
-//         </Link>
-//       </div>
-
-
-// {/* if user is logged in */}
-//       {user && (
-//         <div className="logged-user">
-//           Logged in as {user}
-//           <Logout setUser={setUser} />
-//         </div>
-//       )}
-
-
-// {/* no user logged in */}
-//       <div className="register">
-//         {!user && <Login setUser={setUser} />}
-//         {!user && (
-//           <Link to="/register">
-//             <h3>Register</h3>
-//           </Link>
-//         )}
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Nav;
-
-
-
-
-
 import React, { useState } from "react";
 import "../styles.scss";
 import Login from "./Login";
@@ -75,57 +9,15 @@ function Nav() {
 
   return (
     <nav className="nav">
-      {/* <Link to="/">
-        <h3 id="logo">
-          Veg<i className="fas fa-seedling"></i>able
-        </h3>
-      </Link> */}
-
-
-
-      {/* if user is logged in */}
-      {user && (
-        <div className="logged-in">
+      {/* if user is logged out */}
+      {!user && (
+        <div className="nav-plus-logo">
           <Link to="/">
             <h3 id="logo">
               Veg<i className="fas fa-seedling"></i>able
             </h3>
           </Link>
-          <div className="media">
-            <Link to="/favourites">
-              <h3>
-                <i className="far fa-heart"></i> Favourites
-            </h3>
-            </Link>
-            <Link to="/schedule">
-              <h3>
-                <i className="far fa-calendar-alt"></i> Schedule
-            </h3>
-            </Link>
-            <Link to="/data">
-              <h3>
-                <i className="fas fa-chart-line"></i> Data
-            </h3>
-            </Link>
-          </div>
-          <div className="logged-user">
-            Logged in as {user}
-            <Logout setUser={setUser} />
-          </div>
-        </div>
-      )}
-
-      {/* no user logged in */}
-      {/* <div className="register"> */}
-        {/* {!user && <Login setUser={setUser} />} */}
-        {!user && (
-        <div className="logged-out">
-          <Link to="/">
-            <h3 id="logo">
-              Veg<i className="fas fa-seedling"></i>able
-            </h3>
-          </Link>
-          <div className="login-register-faq">
+          <div className="nav-logged-out">
             <Login setUser={setUser} />
             <div className="register-faq">
               <Link to="/register">
@@ -137,7 +29,49 @@ function Nav() {
             </div>
           </div>
         </div>
-        )}
+      )}
+      {/* if user is logged in */}
+      {user && (
+        <div className="nav-plus-logo logged-in-container">
+          <Link to="/">
+            <h3 id="logo">
+              Veg<i className="fas fa-seedling"></i>able
+            </h3>
+          </Link>
+          <div className="nav-logged-in">
+            <div className="logged-user">
+              Logged in as {user}
+              <Logout setUser={setUser} />
+              <Link to="/faq">
+                <h3>FAQ</h3>
+              </Link>
+            </div>
+            <div className="media">
+              <div className="favourites-media">
+                <Link to="/favourites">
+                  <h3>
+                    <i className="far fa-heart"></i> Favourites
+                  </h3> 
+                </Link>
+              </div>
+              <div className="other-media">
+                <Link to="/schedule">
+                  <h3>
+                    <i className="far fa-calendar-alt"></i> Schedule
+                  </h3>
+                </Link>
+              </div>
+              <div className="other-media">
+              <Link to="/data">
+                <h3>
+                  <i className="fas fa-chart-line"></i> Data
+                </h3>
+              </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
